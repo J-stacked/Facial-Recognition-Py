c602637bea07c6b850e9c7d1557e165494ad576f
J-Stacked <josephehoffmann@outlook.com>
2023-10-21 16:02:17 -0400

    Added image display with bounding boxes for faces

--- detector.py
+++ detector.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import pickle
 from collections import Counter
+from PIL import Image, ImageDraw
 import face_recognition
 
 
@@ -10,6 +11,17 @@ Path("training").mkdir(exist_ok=True)
 Path("output").mkdir(exist_ok=True)
 Path("validation").mkdir(exist_ok=True)
 
+BOUNDING_BOX_COLOR = "blue"
+TEXT_COLOR = "white"
+
+def _display_face(draw, bounding_box, name):
+    top, right, bottom, left = bounding_box
+    draw.rectangle(((left, top), (right, bottom)), outline = BOUNDING_BOX_COLOR)
+    text_left, text_top, text_right, text_bottom = draw.textbbox((left, bottom), name)
+    draw.rectangle(((text_left, text_top), (text_right, text_bottom)), fill = "blue", outline = "blue")
+    draw.text((text_left, text_top), name, fill= "white")
+
+    
 def encode_known_faces(model: str = "hog", encodings_location: Path = DEFAULT_ENCODINGS_PATH) -> None:
     names = []
     encodings = []
@@ -48,11 +60,19 @@ def recognize_faces(image_location: str, model: str = "hog", encodings_location:
     input_image = face_recognition.load_image_file(image_location)
     input_face_locations = face_recognition.face_locations(input_image, model=model)
     input_face_encodings = face_recognition.face_encodings(input_image, input_face_locations)
+
+    pillow_image = Image.fromarray(input_image)
+    drawObj = ImageDraw.Draw(pillow_image)
     
     for bounding_box, unknown_encoding in zip(input_face_locations, input_face_encodings):
         name = _recognize_face(unknown_encoding, loaded_encodings)
         if not name: 
             name = "Unknown"
-        print(name, bounding_box)
+        #print(name, bounding_box)
+        _display_face(drawObj, bounding_box, name)
+
+    del drawObj
+    pillow_image.show()
+        
 
 
